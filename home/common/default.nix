ec18a16059583d299c631bc5a6c722e4cdafe7e2
Dmytro Onypko <vaporif@proton.me>
2026-03-03 20:57:54 +0000

    harden SSH client (modern algos, hash known_hosts) and LibreWolf privacy

--- home/common/default.nix
+++ home/common/default.nix
@@ -217,7 +217,12 @@ in {
       enable = true;
       enableDefaultConfig = false;
       extraOptionOverrides = {
-        StrictHostKeyChecking = "accept-new";
+        StrictHostKeyChecking = "ask";
+        HashKnownHosts = "yes";
+        KexAlgorithms = "curve25519-sha256,curve25519-sha256@libssh.org";
+        HostKeyAlgorithms = "ssh-ed25519,sk-ssh-ed25519@openssh.com";
+        Ciphers = "aes256-gcm@openssh.com,chacha20-poly1305@openssh.com";
+        MACs = "hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com";
       };
       matchBlocks."*" = {
         addKeysToAgent = "yes";

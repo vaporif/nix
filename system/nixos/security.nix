a7dae3269ef4204feaec70aa52b91e777e7309a2
Dmytro Onypko <vaporif@proton.me>
2026-03-03 17:40:01 +0000

    harden nixos: firewall, sudo, umask, sysctl

--- system/nixos/security.nix
+++ system/nixos/security.nix
@@ -2,4 +2,25 @@
   imports = [
     ../../modules/sops.nix
   ];
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [22];
+    allowedUDPPorts = [];
+    logRefusedConnections = true;
+  };
+
+  security.sudo.extraConfig = ''
+    Defaults timestamp_timeout=1
+  '';
+
+  environment.umask = "077";
+
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = false;
+    "net.ipv4.conf.all.accept_redirects" = false;
+    "net.ipv6.conf.all.accept_redirects" = false;
+    "net.ipv4.tcp_syncookies" = true;
+    "kernel.unprivileged_bpf_disabled" = 1;
+  };
 }

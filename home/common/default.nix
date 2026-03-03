ccde08a90d9afaeabaf33c71df0db7a272ab4a5e
Dmytro Onypko <vaporif@proton.me>
2026-03-03 14:23:15 +0000

    notifications + ssh git

--- home/common/default.nix
+++ home/common/default.nix
@@ -165,6 +165,7 @@ in {
         feature.experimental = true;
         help.autocorrect = "prompt";
         branch.sort = "committerdate";
+        url."git@github.com:".insteadOf = "https://github.com/";
         interactive.diffFilter = "delta --color-only";
         delta = {
           navigate = true;

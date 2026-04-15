8c60a2675a577c594275316c40bc41e7154665a3
Dmytro Onypko <vaporif@proton.me>
2026-04-15 23:02:13 +0100

    lsp cleanup

--- config/nvim/lua/core/lsp.lua
+++ config/nvim/lua/core/lsp.lua
@@ -8,10 +8,16 @@ vim.diagnostic.config {
   },
   signs = vim.g.have_nerd_font and {
     text = {
-      [vim.diagnostic.severity.ERROR] = '',
-      [vim.diagnostic.severity.WARN] = '',
-      [vim.diagnostic.severity.HINT] = '',
-      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.HINT] = '',
+      [vim.diagnostic.severity.INFO] = '',
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticError',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticWarn',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
     },
   } or true,
 }

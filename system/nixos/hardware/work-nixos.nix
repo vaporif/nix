99bf97bbc8d24ff35f7e57362edb7d7dcf924381
Dmytro Onypko <vaporif@proton.me>
2026-06-17 19:07:54 +0100

    device ids

--- system/nixos/hardware/work-nixos.nix
+++ system/nixos/hardware/work-nixos.nix
@@ -21,18 +21,16 @@
     extraModulePackages = [];
   };
 
-  # TODO: replace device UUIDs with the values from nixos-generate-config
-  # inside the VMware Fusion guest.
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/63132c8d-b619-42c7-ba7d-c1a4cd8e8581";
+      fsType = "ext4";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/1F09-E40E";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
 
   swapDevices = [];
 

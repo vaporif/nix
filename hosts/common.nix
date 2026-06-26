317e150140b5b3988698e7bf6f54e91be8efdec4
Dmytro Onypko <dmytro.onypko@justmarkets.com>
2026-06-26 11:54:11 +0100

    updates

--- hosts/common.nix
+++ hosts/common.nix
@@ -28,8 +28,8 @@
     };
     user = "vaporif";
     git = {
-      name = "Dmytro Onypko";
-      email = "vaporif@proton.me";
+      name = lib.mkDefault "Dmytro Onypko";
+      email = lib.mkDefault "vaporif@proton.me";
       signingKey = lib.mkDefault "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBP7sf4L6CNhRgdRKmXH2H7xxWTEWMTCS/oOkOwZIIIrpoVeXj01gVp6G4Al0+MdekYO9QbVGX4WMX8+hMpUXs/M= github@secretive.burned-apple.local";
     };
     cachix = {
